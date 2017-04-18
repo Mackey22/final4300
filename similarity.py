cb10ae541107c849fe5fef0b8ff890eee51ceaf1
James Mackey <jjm447@cornell.edu>
2017-04-17 23:36:59 -0400

    Added pruning method

--- similarity.py
+++ similarity.py
@@ -1,8 +1,11 @@
 import json
 import numpy as np
 from collections import defaultdict
+from data_play import make_hist
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import time
+from collections import defaultdict
 
 
 def gen_business_id_to_name():
@@ -17,7 +20,7 @@ business_id_to_name = gen_business_id_to_name()
 
 
 def get_reviews_and_ids():
-    """Return list of unique business_ids, list of concatanted reviews corresponding to list of business_ids."""
+    """Return list of unique business_ids, list of concatenated reviews corresponding to list of business_ids."""
     reviews_map = defaultdict(str)
     count = 0
     with open('yelp_data/yelp_academic_dataset_review.json') as data_file:
@@ -35,16 +38,55 @@ def get_reviews_and_ids():
     for ID,reviews in reviews_map.iteritems():
         ordered_business_ids.append(ID)
         ordered_reviews.append(reviews)
-    print ordered_reviews[0]
+    # print ordered_reviews[0]
     return ordered_business_ids, ordered_reviews
 
 
 # CREATE TFIDF MATRIX
 # UNIQUE_IDS is a list of restaurant ids corresponding to the list of REVIEWS
-unique_ids, reviews = get_reviews_and_ids()
+# start = time.time()
+# unique_ids, reviews = get_reviews_and_ids()
+# mid = time.time()
+# n_feats = 5000
+# tfidf_vec = TfidfVectorizer(max_df=0.8, min_df=10, max_features=n_feats, stop_words='english', norm='l2')
+# restaurant_by_vocab_matrix = tfidf_vec.fit_transform(reviews)
+# end = time.time()
+
+print "Reading"
+start = time.time()
+reviews = defaultdict(int)
+with open('jsons/try1.json') as data_file:
+    data = json.load(data_file)
+
+for k, v in data.items():
+    # print v['review_count']
+    reviews[v['review_count']] += 1
+
+make_hist(reviews, 10, "Review Count", "Review Count", "Count of review Counts")
+
+
+mid = time.time()
+print mid - start
+print "Done Reading"
+
 n_feats = 5000
-tfidf_vec = TfidfVectorizer(max_df=0.8, min_df=3, max_features=n_feats, stop_words='english', norm='l2')
+tfidf_vec = TfidfVectorizer(max_df=0.8, min_df=10, max_features=n_feats, stop_words='english', norm='l2')
 restaurant_by_vocab_matrix = tfidf_vec.fit_transform(reviews)
+end = time.time()
+
+
+print data[data.keys()[0]]
+
+
+def prune_json(n):
+    new_map = {}
+    with open('jsons/try1.json') as data_file:
+        data = json.load(data_file)
+    for k, v in data.items():
+        if v['review_count'] >= n:
+            new_map[k] = v
+    with open('jsons/pruned.json', 'w') as fp:
+        json.dump(new_map, fp, indent=4)
 
 
 def get_sim(vec1, vec2):
@@ -71,7 +113,7 @@ def gen_sim_matrix():
             restaurant_sims[i][j] = 0
     return restaurant_sims
 
-sim_matrix = gen_sim_matrix()
+# sim_matrix = gen_sim_matrix()
 
 
 def find_most_similar(sim_matrix, id1, k=10):
