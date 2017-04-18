520e042aa661154e93713d372a1c34cfc0fe9817
kylealbany <kylealbany@gmail.com>
2017-04-17 23:32:49 -0400

    Working on similarity.py, created file for business id to name dict

--- similarity.py
+++ similarity.py
@@ -8,26 +8,48 @@ from sklearn.metrics.pairwise import cosine_similarity
 def gen_business_id_to_name():
     """Return Dict - maps business id to business name."""
     business_id_to_name = defaultdict(str)
-    with open('yelp_data/yelp_academic_dataset_business.json') as data_file:
+    with open('yelp_academic_dataset_business.json') as data_file:
         for line in data_file:
             data = (json.loads(line))
             business_id_to_name[data['business_id']] = data['name']
     return business_id_to_name
 business_id_to_name = gen_business_id_to_name()
 
+# Old version of the function parses the original Yelp dataset file
+# def get_reviews_and_ids_old():
+#     """Return list of unique business_ids, list of concatanted reviews corresponding to list of business_ids."""
+#     reviews_map = defaultdict(str)
+#     count = 0
+#     with open('yelp_data/yelp_academic_dataset_review.json') as data_file:
+#         for line in data_file:
+#             # currently limits size as it won't run on my machine otherwise
+#             # might speed it up to use .join instead, but I think mackey has code for this portion already anyway
+#             if count < 2000:
+#                 data = (json.loads(line))
+#                 reviews_map[data['business_id']] += data['text']
+#             count += 1
+
+#     ordered_business_ids = []
+#     ordered_reviews = []
+
+#     for ID,reviews in reviews_map.iteritems():
+#         ordered_business_ids.append(ID)
+#         ordered_reviews.append(reviews)
+#     print ordered_reviews[0]
+#     return ordered_business_ids, ordered_reviews
+
 
 def get_reviews_and_ids():
     """Return list of unique business_ids, list of concatanted reviews corresponding to list of business_ids."""
     reviews_map = defaultdict(str)
     count = 0
-    with open('yelp_data/yelp_academic_dataset_review.json') as data_file:
-        for line in data_file:
-            # currently limits size as it won't run on my machine otherwise
-            # might speed it up to use .join instead, but I think mackey has code for this portion already anyway
-            if count < 2000:
-                data = (json.loads(line))
-                reviews_map[data['business_id']] += data['text']
-            count += 1
+    with open('reviews.json') as data_file:
+        data = json.load(data_file)
+    for key in data:
+        count += 1
+        if count > 10:
+            break
+        reviews_map[key] = data[key]['reviews']
 
     ordered_business_ids = []
     ordered_reviews = []
@@ -38,14 +60,15 @@ def get_reviews_and_ids():
     print ordered_reviews[0]
     return ordered_business_ids, ordered_reviews
 
-
 # CREATE TFIDF MATRIX
 # UNIQUE_IDS is a list of restaurant ids corresponding to the list of REVIEWS
+print("before get_reviews_and_ids() call\n")
 unique_ids, reviews = get_reviews_and_ids()
+print("after get_reviews_and_ids() call\n")
 n_feats = 5000
 tfidf_vec = TfidfVectorizer(max_df=0.8, min_df=3, max_features=n_feats, stop_words='english', norm='l2')
 restaurant_by_vocab_matrix = tfidf_vec.fit_transform(reviews)
-
+print("tfidf vectorizer done \n")
 
 def get_sim(vec1, vec2):
     """
@@ -67,12 +90,13 @@ def gen_sim_matrix():
         for j in range(len(unique_ids)):
             if i != j:
                 restaurant_sims[i][j] = get_sim(restaurant_by_vocab_matrix[i], restaurant_by_vocab_matrix[j])
-        else:
-            restaurant_sims[i][j] = 0
+            else:
+                restaurant_sims[i][j] = 0
     return restaurant_sims
 
+print("About to generate sim matrix\n")
 sim_matrix = gen_sim_matrix()
-
+print("Generated sim matrix\n")
 
 def find_most_similar(sim_matrix, id1, k=10):
     """
