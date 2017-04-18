23d87edb6d35acc8891b1e994ef793a2e9927f45
kylealbany <kylealbany@gmail.com>
2017-04-18 14:49:33 -0400

    Modified gen_business_maps to use yelp_data folder

--- gen_business_maps.py
+++ gen_business_maps.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 def gen_business_id_to_name():
     """Return Dict - maps business id to business name."""
     business_id_to_name = defaultdict(str)
-    with open('yelp_academic_dataset_business.json') as data_file:
+    with open('yelp_data/yelp_academic_dataset_business.json') as data_file:
         for line in data_file:
             data = (json.loads(line))
             business_id_to_name[data['business_id']] = data['name'].lower()
@@ -15,7 +15,7 @@ def gen_business_id_to_name():
 def gen_business_name_to_id():
     """Return Dict - maps business names to business ids."""
     business_name_to_id = defaultdict(str)
-    with open('yelp_academic_dataset_business.json') as data_file:
+    with open('yelp_data/yelp_academic_dataset_business.json') as data_file:
         for line in data_file:
             data = (json.loads(line))
             business_name_to_id[data['name'].lower()] = data['business_id']
