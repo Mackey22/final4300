6332f2f6f4f4fe9775e754306103703431ca1c55
Mackey22 <jjm447@cornell.edu>
2017-04-27 23:15:35 -0400

    Adding to not be overwritten

--- project_template/test.py
+++ project_template/test.py
@@ -11,31 +11,28 @@ from helpers import sort_dict_by_val
 import requests
 
 
-#YELP API
-app_id ='xGJw03IyUyyPy4XuNn4h_A'
+# YELP API
+app_id = 'xGJw03IyUyyPy4XuNn4h_A'
 app_secret = 'HfiPbyE1t1tzH7NkEJ5e1kcG157gw1uQ15BX4YhiQCpJjHyAR34T3AUuP1aU2nz9'
 data = {'grant_type': 'client_credentials',
         'client_id': app_id,
         'client_secret': app_secret}
 token = requests.post('https://api.yelp.com/oauth2/token', data=data)
-access_token = token.json()['access_token']       
+access_token = token.json()['access_token']
 url = 'https://api.yelp.com/v3/businesses/search'
 headers = {'Authorization': 'bearer %s' % access_token}
 
 
 def api_business_info(business_name, location):
-    #remove apostrophes and spaces to avoid errors in api call
+    # remove apostrophes and spaces to avoid errors in api call
     business_name = business_name.replace("'","")
     location = location.replace("'","")
-    params = {'location': location,
-          'term': business_name,
-         }
+    params = {'location': location, 'term': business_name}
     url = 'https://api.yelp.com/v3/businesses/search'
     # url = 'https://api.yelp.com/v3/businesses/' + business_name + "-" + location + "/reviews"
     resp = requests.get(url=url, params=params, headers=headers)
     top_business = resp.json()['businesses'][0]
     return top_business
-
 
 
 # k is number of results to display
@@ -46,16 +43,16 @@ def find_most_similar(topMatches, unique_ids, business_id_to_name, id1, destCity
     Accepts: similarity matrix of restauranst, restaurant id.
     Returns: list of (score, restaurant name) tuples for restaurant with id1 sorted by cosine_similarity score
     """
-    #rel_index = unique_ids.index(id1)
-    #rel_row = sim_matrix[rel_index]
-    #print "rel_index: "
-    #print rel_index
-    #print "destCity: "
-    #print destCity
+    # rel_index = unique_ids.index(id1)
+    # rel_row = sim_matrix[rel_index]
+    # print "rel_index: "
+    # print rel_index
+    # print "destCity: "
+    # print destCity
     topMatchesRow = topMatches[id1][destCity]
-    #max_indices = np.argpartition(rel_row, -k)[-k:]
-    #most_similar_scores_and_ids = [(rel_row[x], business_id_to_name[unique_ids[x]]) for x in max_indices]
-    #most_similar_scores_and_ids = sorted(most_similar_scores_and_ids,key=lambda x:-x[0])
+    # max_indices = np.argpartition(rel_row, -k)[-k:]
+    # most_similar_scores_and_ids = [(rel_row[x], business_id_to_name[unique_ids[x]]) for x in max_indices]
+    # most_similar_scores_and_ids = sorted(most_similar_scores_and_ids,key=lambda x:-x[0])
     most_similar_ids = [business_id_to_name[x] for x in topMatchesRow][:k]
     # id -> (name,city,state)
     res = []
@@ -74,7 +71,7 @@ def find_most_similar(topMatches, unique_ids, business_id_to_name, id1, destCity
 
 def get_ordered_cities():
     data = read(1)["cities"]
-    #Deal with Montréal and other accent problems here
+    # Deal with Montréal and other accent problems here
     for i in range(len(data)):
         data[i] = data[i].replace(u'\xe9', 'e')
     return sorted(data[:10]), (["Search in all cities"] + sorted(data))
@@ -91,15 +88,15 @@ def read_file(n):
     path = Docs.objects.get(id=n).address
     file = open(path)
     data = json.load(file)
-    #sim_matrix = data['svd_matrix']
+    # sim_matrix = data['svd_matrix']
     topMatches = data['topMatches']
     unique_ids = data['unique_ids']
     business_id_to_name = data['business_id_to_name']
     business_name_to_id = data['business_name_to_id']
-    #print "length sim matrix: " + str(len(sim_matrix))
+    # print "length sim matrix: " + str(len(sim_matrix))
     print "length unique_ids: " + str(len(unique_ids))
     print unique_ids[0]
-    #print business_id_to_name[unique_ids[0]]
+    # print business_id_to_name[unique_ids[0]]
     return topMatches, unique_ids, business_id_to_name, business_name_to_id
 
 
